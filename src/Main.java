35c501c4d51ce96b86372b814c7d93ef67d5322c
sschafe <sschafe2@emich.edu>
2021-08-15 18:54:53 -0400

    Added coupons, sales tax, shipping, and final output

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,13 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		
-		System.out.println("Please input number of items: ");
+		//Cart
+		System.out.println("Please input the number of items: ");
 		int items = input.nextInt();
 		
 		double [] cart = new double[items];
 		
-		System.out.println("Please cost of each item: ");
+		System.out.println("Please enter the cost of each item: ");
 		for(int i = 0; i < items; i++) {
 			cart[i] = input.nextDouble();
 		}
@@ -20,6 +21,49 @@ public class Main {
 //		for(int i = 0; i < items; i++) {
 //			System.out.println(cart[i]);
 //		}
+		
+		//Coupons
+		System.out.println("Please enter the number of coupons that you have: ");
+		int numberCoupons = input.nextInt();
+		
+		double [] coupons = new double[numberCoupons];
+		System.out.println("Please enter each of your coupons: ");
+		for(int i = 0; i < numberCoupons; i++) {
+			coupons[i] = input.nextDouble();
+		}
+		
+		System.out.println();
+		
+		//Sales tax
+		System.out.println("Please enter the sales tax: ");
+		double sales = input.nextDouble();
+		
+		System.out.println();
+		
+		//Shipping
+		System.out.println("Please enter the shipping cost: ");
+		double shipping = input.nextDouble();
+		
+		System.out.println();
+		
+		//Final cost
+		//Cost of items
+		double preCost = 0.0;
+		for(int i = 0; i < items; i++) {
+			preCost = preCost + cart[i];
+		}
+		
+		//Coupons
+		double off = 0.0;
+		for(int i = 0; i < numberCoupons; i++) {
+			off = off + coupons[i];
+		}
+		
+		double costSales = (preCost - off)*sales;
+		
+		double finalCost = preCost - off + costSales + shipping;
+		
+		System.out.println("The cost of the order is: $" + finalCost);
 	}
 
 }
